515f6c437a4a1bf6118e09942f32c86d070e7d92
ssKazal <salmansiddik19@outlook.com>
2023-10-01 19:00:58 +0600

    #WIP-7 | CT feedback > abstrack model

--- user/models/auth/profile.py
+++ user/models/auth/profile.py
@@ -6,7 +6,7 @@ from .user import User
 from utils.general_data import UNIQUE_PROFILE_EMAIL_MESSAGE
 
 
-class DeveloperProfile(TimestampedModel):
+class Profile(TimestampedModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(
         db_collation="case_insensitive",
@@ -15,47 +15,31 @@ class DeveloperProfile(TimestampedModel):
         error_messages={"unique": UNIQUE_PROFILE_EMAIL_MESSAGE},
     )
     phone_number = PhoneNumberField(null=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return str(self.user)
+
+
+class DeveloperProfile(Profile):
     company_logo = models.ImageField(max_length=1000, null=True, upload_to="developer_profile_company_logo/")
     company_name = models.CharField(max_length=100, null=True)
     company_address = models.CharField(max_length=500, null=True)
     company_details = models.CharField(max_length=2000, null=True)
 
-    def __str__(self):
-        return str(self.user)
 
-
-class AgentProfile(TimestampedModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(
-        db_collation="case_insensitive",
-        unique=True,
-        null=True,
-        error_messages={"unique": UNIQUE_PROFILE_EMAIL_MESSAGE},
-    )
-    phone_number = PhoneNumberField(null=True)
+class AgentProfile(Profile):
     company_logo = models.ImageField(max_length=1000, null=True, upload_to="agent_profile_company_logo/")
     company_name = models.CharField(max_length=100, null=True)
     company_address = models.CharField(max_length=500, null=True)
     company_details = models.CharField(max_length=2000, null=True)
 
-    def __str__(self):
-        return str(self.user)
 
-
-class ProspectProfile(TimestampedModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class ProspectProfile(Profile):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(
-        db_collation="case_insensitive",
-        unique=True,
-        null=True,
-        error_messages={"unique": UNIQUE_PROFILE_EMAIL_MESSAGE},
-    )
-    phone_number = PhoneNumberField(null=True)
     picture = models.ImageField(max_length=1000, null=True, upload_to="prospect_profile_picture/")
     gender = models.CharField(max_length=50, choices=GENDER, null=True)
     address = models.CharField(max_length=2000, null=True)
-
-    def __str__(self):
-        return str(self.user)
