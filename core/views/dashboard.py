a2034d2e94617e434c32f683d91c95620af01272
ssKazal <salmansiddik19@outlook.com>
2023-10-02 17:44:04 +0600

    Separate user type dashboard

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 
 @login_required
 def dashboard(request):
-    return render(request, "core/dashboard.html")
+    if request.user.user_type == "developer" or request.user.user_type == "agent":
+        to_return = developer_or_agent_dashboard(request)
+    elif request.user.user_type == "prospect":
+        to_return = prospect_dashboard(request)
+
+    return to_return
+
+
+@login_required
+def developer_or_agent_dashboard(request):
+    return render(request, "core/developer_or_agent_dashboard.html")
+
+
+@login_required
+def prospect_dashboard(request):
+    return render(request, "core/prospect_dashboard.html")
