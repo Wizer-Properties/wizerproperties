9fc49b5935823d1c6a6b9f6ea62932e0d630d507
ssKazal <salmansiddik19@outlook.com>
2023-10-07 12:42:32 +0600

    Remane static root path

--- wizerproperties/settings/base.py
+++ wizerproperties/settings/base.py
@@ -134,7 +134,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR.parent, 'static'),)
 
