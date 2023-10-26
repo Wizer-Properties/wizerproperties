f6ea09d58b7ab7a4532da1bee2a7775839596b34
ssKazal <salmansiddik19@outlook.com>
2023-10-26 13:02:57 +0600

    Fix Image upload issue

--- src/building/api/serializers.py
+++ src/building/api/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from building.models import Building, BuildingMedia
 from utils.general_func import show_custom_error_message
@@ -103,32 +104,48 @@ class BuildingSerializer(serializers.ModelSerializer):
         return building
 
     def update(self, instance, validated_data):
+        media_files_data = self.get_media_files(self.request)
         deleted_images = self.request.data.getlist("deleted_images")
 
-        # Check if all deleted_images are deletable
-        remaining_file_types = instance.media_files.exclude(id__in=deleted_images).values_list("type", flat=True)
+        # Reverting any changes made to the instance media files field, If no exception occurs the changes will be committed when the block exits
+        with transaction.atomic():
+            try:
+                # Update BuildingMedia objects for different media types
+                for media_type, files in media_files_data.items():
+                    for file in files:
+                        media_file = BuildingMedia(type=media_type, file=file)
+                        media_file.save()
+                        instance.media_files.add(media_file)
 
-        if (
-            "image" not in remaining_file_types
-            or "floor_plan" not in remaining_file_types
-            or "unit_floor_plan" not in remaining_file_types
-            or "master_plan" not in remaining_file_types
-            or "video" not in remaining_file_types
-        ):
-            raise serializers.ValidationError(
-                {"media_files": ["At least one image every type of media file must remain with the building."]}
-            )
-        else:
-            # Delete the deletable images
-            instance.media_files.filter(id__in=deleted_images).delete()
+                # Check if all deleted_images are deletable
+                remaining_file_types = instance.media_files.exclude(id__in=deleted_images).values_list(
+                    "type", flat=True
+                )
 
-        media_files_data = self.get_media_files(self.request)
+                if (
+                    "image" not in remaining_file_types
+                    or "floor_plan" not in remaining_file_types
+                    or "unit_floor_plan" not in remaining_file_types
+                    or "master_plan" not in remaining_file_types
+                    or "video" not in remaining_file_types
+                ):
+                    raise serializers.ValidationError(
+                        {
+                            "media_files": [
+                                "At least one image of every type of media file must remain with the building."
+                            ]
+                        }
+                    )
 
-        # Update BuildingMedia objects for different media types
-        for media_type, files in media_files_data.items():
-            for file in files:
-                media_file = BuildingMedia(type=media_type, file=file)
-                media_file.save()
-                instance.media_files.add(media_file)
+                # Delete the deletable images
+                instance.media_files.filter(id__in=deleted_images).delete()
+
+            except Exception as e:
+                # Handle the exception if needed
+                raise e
+
+        # To remain unchange active status while Edit
+        if self.request.method == "PUT":
+            validated_data.pop("is_active", None)
 
         return super().update(instance, validated_data)
--- src/property/api/serializers.py
+++ src/property/api/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from property.models import Property, PropertyMedia, CompareProperty
 from building.api.serializers import BuildingSerializer
@@ -46,12 +47,6 @@ class PropertySerializer(serializers.ModelSerializer):
         super(PropertySerializer, self).__init__(*args, **kwargs)
         self.request = self.context.get("request")
 
-        self.media_files_data = {
-            "image": self.request.FILES.getlist("images"),
-            "unit_plan": self.request.FILES.getlist("unit_plans"),
-            "video": self.request.FILES.getlist("videos"),
-        }
-
         for field_name, field in self.fields.items():
             if self.instance is not None and field_name in ["images", "unit_plans", "videos"]:
                 field.required = False
@@ -62,10 +57,19 @@ class PropertySerializer(serializers.ModelSerializer):
 
         show_custom_error_message(self.fields)
 
+    def get_media_files(self, request):
+        return {
+            "image": self.request.FILES.getlist("images"),
+            "unit_plan": self.request.FILES.getlist("unit_plans"),
+            "video": self.request.FILES.getlist("videos"),
+        }
+
     def create(self, validated_data):
+        media_files_data = self.get_media_files(self.request)
+
         # Create PropertyMedia objects for different media types
         media_files = []
-        for media_type, files in self.media_files_data.items():
+        for media_type, files in media_files_data.items():
             for file in files:
                 media_file = PropertyMedia(type=media_type, file=file)
                 media_file.save()
@@ -82,29 +86,43 @@ class PropertySerializer(serializers.ModelSerializer):
         return property
 
     def update(self, instance, validated_data):
+        media_files_data = self.get_media_files(self.request)
         deleted_images = self.request.data.getlist("deleted_images")
 
-        # Check if all deleted_images are deletable
-        remaining_file_types = instance.media_files.exclude(id__in=deleted_images).values_list("type", flat=True)
+        # Reverting any changes made to the instance media files field, If no exception occurs the changes will be committed when the block exits
+        with transaction.atomic():
+            try:
+                # Update PropertyMedia objects for different media types
+                for media_type, files in media_files_data.items():
+                    for file in files:
+                        media_file = PropertyMedia(type=media_type, file=file)
+                        media_file.save()
+                        instance.media_files.add(media_file)
 
-        if (
-            "image" not in remaining_file_types
-            or "unit_plan" not in remaining_file_types
-            or "video" not in remaining_file_types
-        ):
-            raise serializers.ValidationError(
-                {"media_files": ["At least one image every type of media file must remain with the property."]}
-            )
-        else:
-            # Delete the deletable images
-            instance.media_files.filter(id__in=deleted_images).delete()
+                # Check if all deleted_images are deletable
+                remaining_file_types = instance.media_files.exclude(id__in=deleted_images).values_list(
+                    "type", flat=True
+                )
 
-        # Update PropertyMedia objects for different media types
-        for media_type, files in self.media_files_data.items():
-            for file in files:
-                media_file = PropertyMedia(type=media_type, file=file)
-                media_file.save()
-                instance.media_files.add(media_file)
+                if (
+                    "image" not in remaining_file_types
+                    or "unit_plan" not in remaining_file_types
+                    or "video" not in remaining_file_types
+                ):
+                    raise serializers.ValidationError(
+                        {"media_files": ["At least one image every type of media file must remain with the property."]}
+                    )
+
+                # Delete the deletable images
+                instance.media_files.filter(id__in=deleted_images).delete()
+
+            except Exception as e:
+                # Handle the exception if needed
+                raise e
+
+        # To remain unchange active status while Edit
+        if self.request.method == "PUT":
+            validated_data.pop("is_active", None)
 
         return super().update(instance, validated_data)
 
