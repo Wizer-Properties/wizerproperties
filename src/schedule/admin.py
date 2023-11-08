9924605b9905a4d291aac6b008f981e459a0e8a7
Kanon <gentlekanon@gmail.com>
2023-11-08 19:14:58 +0600

    #WIP | Hide recent action section and add api doc

--- src/schedule/admin.py
+++ src/schedule/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from schedule.models import VisitingSchedule
 
-# Register your models here.
+
+@admin.register(VisitingSchedule)
+class VisitingScheduleAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "visiting_time",
+        "status",
+        "prospect",
+        "content_type",
+        "object_id",
+        "content_object",
+        "created_at",
+        "updated_at",
+    ]
