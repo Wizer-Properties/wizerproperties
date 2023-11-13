1d8f18edca39d676f6f78de55548de48966cd3d0
ssKazal <salmansiddik19@outlook.com>
2023-11-13 13:23:24 +0600

    Compare property clean method

--- src/property/models/compare_property.py
+++ src/property/models/compare_property.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from core.models import TimestampedModel
 
 
@@ -11,3 +12,12 @@ class CompareProperty(TimestampedModel):
 
     def __str__(self):
         return f"{self.user} - {self.property.title}"
+
+    def clean(self):
+        # Check if there is already an object with the same user and property
+        existing_objects = self.__class__.objects.filter(user=self.user, property=self.property)
+        if self.id:
+            existing_objects = existing_objects.exclude(id=self.id)  # Exclude the current object for updates
+
+        if existing_objects.exists():
+            raise ValidationError("A comparison already exists for this user and property.")
