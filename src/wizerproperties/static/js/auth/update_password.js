5765d9af3a16600421b6ef91025e12e739f3b5b5
mehedi23 <satusatusatu0@gmail.com>
2024-02-13 10:31:16 +0600

    #WIP-44 | Login/Home page design update

--- src/wizerproperties/static/js/auth/update_password.js
+++ src/wizerproperties/static/js/auth/update_password.js
@@ -1,13 +1,31 @@
 $(document).ready(function () {
-    $(".authButton").click(function () {
-        var password1 = $("#password1").val();
-        var password2 = $("#password2").val();
+    var sent_request = false;
+    var loader_dom = '<div id="loadingSpinner" class="spinner-border" role="status">'+
+                        '<span class="sr-only">Loading...</span>' +
+                      '</div>'
 
-        var updatePasswordButtonText = $('#updatePasswordButtonText');
-        var loadingSpinner = $('#loadingSpinner');
+    $('[button-name="update-password"]').click(function () {
+        if(sent_request) return;
+        var password1 = $('[name="password1"]').val();
+        var password2 = $('[name="password2"]').val();
 
-        updatePasswordButtonText.hide(); // Hide the text
-        loadingSpinner.show() // Show the spinner
+        if(
+            password1.trim() == '' ||
+            password2.trim() == ''
+        ){
+            var msg_dom = '<div class="alert alert-danger p-2" role="alert">Fill up the passwords field</div>'
+            $('.auth-response-messages').html(msg_dom);
+            return;
+        };
+
+        if( password1 != password2  ){
+            var msg_dom = '<div class="alert alert-danger p-2" role="alert">Password not matching</div>'
+            $('.auth-response-messages').html(msg_dom);
+            return;
+        };
+
+        $(this).html(loader_dom);
+        sent_request = true;
 
         $.ajax({
             type: "POST",
@@ -20,11 +38,8 @@ $(document).ready(function () {
                 "X-CSRFToken": csrfToken,
             },
             success: function (response) {
-                loadingSpinner.hide(); // Hide the spinner
-                updatePasswordButtonText.show(); // Show the text
-
-                $(".authErrorMessage").text("");
-                $(".authSuccessMessage").text(response.message);
+                var msg_dom = '<div class="alert alert-success p-2" role="alert">'+ response.message +'</div>'
+                $('.auth-response-messages').html(msg_dom)
 
                 // Redirect to the homepage after 1 second
                 setTimeout(function () {
@@ -32,16 +47,19 @@ $(document).ready(function () {
                 }, 1000);
             },
             error: function (error) {
-                loadingSpinner.hide(); // Hide the spinner
-                updatePasswordButtonText.show(); // Show the text
-
                 errorMessages = ""
                 for (let i = 0; i < error.responseJSON.message.length; i++) {
                     errorMessages += error.responseJSON.message[i]
                 }
-                $(".authSuccessMessage").text("");
-                $(".authErrorMessage").html(errorMessages);
+
+                var msg_dom = '<div class="alert alert-danger p-2" role="alert">'+ errorMessages +'</div>'
+                $('.auth-response-messages').html(msg_dom)
+                
             },
+            complete: function(){
+                sent_request = false;
+                $('[button-name="update-password"]').html('Save')
+            }
         });
     });
 });
