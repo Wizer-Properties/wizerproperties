d2e5ea6e87db971dbabbc28d92482fad8f540d9f
mehedi23 <satusatusatu0@gmail.com>
2024-03-28 11:16:09 +0600

    #WIP-64 | Design Most Engaging and Informative Reels > Make form

--- src/advertise/urls.py
+++ src/advertise/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-
+from .views import create_reels
 
 urlpatterns = [
+    path("create-reels/", create_reels, name="create_reels"),
     path("api/", include(("advertise.api.urls", "advertise"), namespace="api")),
 ]
--- src/advertise/views.py
+++ src/advertise/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+@login_required
+def create_reels(request):
+    return render(request, "create-reels.html")
