db56227327dc561fec22dea891641b5edf56afe5
ssKazal <salmansiddik19@outlook.com>
2024-05-29 15:52:58 +0600

    #WIP-83 | Update 'Trending Developments- The Most Searched' list

--- src/property/models/__init__.py
+++ src/property/models/__init__.py
@@ -2,6 +2,5 @@ from .default import Property
 from .media import PropertyMedia
 from .compare import CompareProperty
 from .favorite import ProspectFavoriteProperty
-from .popular import PopularProperty
 from .newly_created import NewlyCreatedProperty
 from .discount import DiscountProperty
