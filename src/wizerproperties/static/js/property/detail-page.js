544efc1575e8540900ba4236241f0a0cbb065993
armanmalikk <armanmalikk14@gmail.com>
2024-07-15 09:02:49 +0600

    small update on the detail page

--- src/wizerproperties/static/js/property/detail-page.js
+++ src/wizerproperties/static/js/property/detail-page.js
@@ -788,7 +788,7 @@ $(document).ready(function(){
                 $('[label-name="company_logo"]')
                 .html('<img src="'+data?.company_logo+'" alt="building img" loading="lazy"/>')
                 $('[label-name="company_name"').html(data?.company_name)
-                $('[label-name="address"').html(data?.address)
+                //$('[label-name="address"').html(data?.address)
                 $('[label-name="phone_number"]').html('<a href="tel:'+data?.phone_number+'">'+data?.phone_number+'</a>')
                 $('[label-name="email"]').html('<a href="mailto:'+data?.email+'">'+data?.email+'</a>')
 
